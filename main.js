3542732b93ed547d5800628b7e5d465f40a62f47
whatisjohnny <docklocket@gmail.com>
2022-09-16 11:17:57 -0600

    Pizza Order

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-// JavaScript Code
+
+alert("Hello!")
+alert("Can I take your order?")
+
+
+//input
+let size = prompt("What size pizza would you like?");
+let topping1 = prompt("What is your first topping?");
+let topping2 = prompt("What is your second topping?");
+
+
+//process
+let msg = `Your ${size} pizza with ${topping1} and ${topping2} will be ready in 20 minutes!`
+
+//output
+alert(msg);
+
+
+
+
+
